76e933bdfcf9dd50c491d2c41b236584605c82a1
Ethan Aquino <ethan.aquino.258@gmail.com>
2021-05-23 18:45:52 -0500

    added more views and separated them out

--- songs/urls.py
+++ songs/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from django.urls import path
 from . import views
 
+app_name='songs'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('topTracks/', views.topTracks, name='topTracks'),
+    path('recentlyPlayed/', views.recentlyPlayed, name='recentlyPlayed'),
+    path('topArtists/', views.topArtists, name='topArtists'),
 ]
--- songs/views.py
+++ songs/views.py
@@ -7,15 +7,43 @@ from .forms import termForm
 
 # Create your views here.
 def index(request):
+    return render(request, 'songs/index.html')
+
+def topTracks(request):
     if request.method == 'POST':
         form = termForm(request.POST)
         if form.is_valid():
             client = authCode("user-top-read playlist-modify-public", request.POST.get('username'))
-            print("============")
-            print(request.POST.get('term_length'))
             results = client.current_user_top_tracks(limit=50,time_range=request.POST.get('term_length'))
             tracks = results['items']
-            return render(request, 'songs/index.html', {'results': tracks})
+            return render(request, 'songs/topTracks.html', {'results': tracks})
     else:
         form = termForm()
-    return render(request, 'songs/index.html', {'form': form})
+    return render(request, 'songs/topTracks.html', {'form': form})
+
+def recentlyPlayed(request):
+    if request.method == 'POST':
+        client = authCode("user-read-recently-played", request.POST.get('username'))
+        results = client.current_user_recently_played()
+        recentResults = results['items']
+
+        tracks = []
+        for item in recentResults:
+            tracks.append(item['track'])
+
+        return render(request, 'songs/recentlyPlayed.html', {'results': tracks})
+    else:
+        return render(request, 'songs/recentlyPlayed.html')
+
+def topArtists(request):
+    if request.method == 'POST':
+        form = termForm(request.POST)
+        if form.is_valid():
+            client = authCode("user-top-read playlist-modify-public", request.POST.get('username'))
+            results = client.current_user_top_artists(limit=50, time_range=request.POST.get('term_length'))
+            artists = results['items']
+            return render(request, 'songs/topArtists.html', {'results': artists})
+    else:
+        form = termForm()
+    return render(request, 'songs/topArtists.html', {'form': form})
+
